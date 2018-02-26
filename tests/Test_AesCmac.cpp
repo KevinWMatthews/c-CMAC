d996e99e76832c5655db52d500283e819ecc3b8c
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-02-25 16:37:11 -0800

    Add skeleton of CMake build system with unit tests

---
+++ tests/Test_AesCmac.cpp
@@ -0,0 +1,21 @@
+extern "C"
+{
+}
+
+#include "CppUTest/TestHarness.h"
+
+TEST_GROUP(aes_cmac)
+{
+    void setup()
+    {
+    }
+
+    void teardown()
+    {
+    }
+};
+
+TEST(aes_cmac, wiring_check)
+{
+    FAIL("start here");
+}
