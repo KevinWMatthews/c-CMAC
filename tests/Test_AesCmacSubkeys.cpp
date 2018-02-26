1c21cec810906fb7828a8253ff45a5acdebd9def
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-02-26 08:25:23 -0800

    Expect call to mock AES library - failing test

--- tests/Test_AesCmacSubkeys.cpp
+++ tests/Test_AesCmacSubkeys.cpp
@@ -4,15 +4,19 @@ extern "C"
 }
 
 #include "CppUTest/TestHarness.h"
+#include "CppUTestExt/MockSupport.h"
 
 TEST_GROUP(aes_cmac_subkeys)
 {
     void setup()
     {
+        mock().strictOrder();
     }
 
     void teardown()
     {
+        mock().checkExpectations();
+        mock().clear();
     }
 };
 
@@ -31,9 +35,30 @@ TEST(aes_cmac_subkeys, generate_subkeys_for_rfc_examples)
         0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6,
         0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c,
     };
+    uint8_t iv[] = {
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    };
+    uint8_t input[] = {
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    };
+
     uint8_t actual_K1[16] = {};
     uint8_t actual_K2[16] = {};
     int ret;
+
+    uint8_t aes_128[] = {
+        0x7d, 0xf7, 0x6b, 0x0c, 0x1a, 0xb8, 0x99, 0xb3,
+        0x3e, 0x42, 0xf0, 0x47, 0xb9, 0x1b, 0x54, 0x6f,
+    };
+
+    mock().expectOneCall("Aes_Calculate128")
+        .withParameter("key", key)
+        .withParameter("iv", iv)
+        .withParameter("input", iv)
+        .withOutputParameterReturning("output", aes_128, sizeof(aes_128))
+        .andReturnValue(0);
 
     ret = AesCmac_GenerateSubkeys(key, actual_K1, actual_K2);
 
