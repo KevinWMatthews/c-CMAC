87aafea5230ae4dc5b3f7778908386f57c4a0295
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-02-27 08:25:10 -0800

    Map out bitshifting step

--- include/AesCmacSubkeys.h
+++ include/AesCmacSubkeys.h
@@ -4,6 +4,9 @@
 #include <stdint.h>
 #include "Aes.h"
 
+// Do I want to change this AES_KEY parameter? Probably.
+// CMAC forces the IV to 0, so the high level shouldn't be allowed to set it.
+// Figure out what layer needs to know this detail.
 int AesCmac_GenerateSubkeys(AES_KEY_128 *aes_key, uint8_t K1[16], uint8_t K2[16]);
 
 #endif
--- src/AesCmacSubkeys.c
+++ src/AesCmacSubkeys.c
@@ -2,10 +2,43 @@
 #include "Aes.h"
 #include <string.h>
 
-uint8_t const_Zero[16] = {
+// See RFC4493, Fig 2.2
+static uint8_t const_Zero[16] = {
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 };
+
+// See RFC4493, Fig 2.2
+static uint8_t const_Rb[16] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x87,
+};
+
+/*
+ * Shift the bits in an array to the left by one.
+ * Bytes in the array are assumed to be stored with
+ * the least significant byte first?
+ *
+ * So:
+ *  0xffee..1100
+ *    ||      -- LSB
+ *    -- MSB
+ * is stored as:
+ *  00 11 .. ee ff
+ *
+ * Bitshifting:
+ *  0x00        0x11        0xX     0xee        0xff
+ *  0b0000 0000 0b0001 0001 0bX .. 0b1110 1110 0b1111 1111
+ * yields:
+ *  0x00        0x22               0xdd        0xfe
+ *  0b0000 0000 0b0010 001X .. 0b1 0b1101 1101 0b1111 1110
+ *                                                       - This bit wraps around.
+ */
+//TODO test this...
+static void bitshift_left_one(uint8_t *input, size_t input_len,
+                              uint8_t *output, size_t output_len)
+{
+}
 
 int AesCmac_GenerateSubkeys(AES_KEY_128 *aes_key, uint8_t K1[16], uint8_t K2[16])
 {
