79a93925553d2f819c2faf527f7a7c691f7c072d
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-03-03 07:39:00 -0800

    Naively shift a single bit

--- src/BitOperation.c
+++ src/BitOperation.c
@@ -4,5 +4,6 @@ int BitOperation_CircularShiftLeft(uint8_t bits_to_shift,
         uint8_t *input, size_t input_len,
         uint8_t *output, size_t output_len)
 {
+    output[0] = input[0] << bits_to_shift;
     return 0;
 }
--- tests/Test_BitOperation.cpp
+++ tests/Test_BitOperation.cpp
@@ -7,6 +7,8 @@ extern "C"
 
 TEST_GROUP(LeftShiftOne)
 {
+    int ret;
+
     void setup()
     {
     }
@@ -21,7 +23,18 @@ TEST(LeftShiftOne, shift_on_zero_byte_has_no_effect)
     uint8_t expected[1] = {0};
     uint8_t input[1] = {0};
     uint8_t output[1] = {0};
-    int ret;
+
+    ret = BitOperation_CircularShiftLeft( 1, input, sizeof(input), output, sizeof(output) );
+
+    MEMCMP_EQUAL( expected, output, sizeof(expected) );
+    LONGS_EQUAL( 0, ret );
+}
+
+TEST(LeftShiftOne, shift_a_single_bit)
+{
+    uint8_t expected[1] = {0b10};
+    uint8_t input[1] = {0b1};
+    uint8_t output[1] = {0};
 
     ret = BitOperation_CircularShiftLeft( 1, input, sizeof(input), output, sizeof(output) );
 
