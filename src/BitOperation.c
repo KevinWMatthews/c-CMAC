38007dae9b44df271ee2a180e93701592be6676b
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-03-03 10:01:45 -0800

    Rewrite bit shifting using a loop

--- src/BitOperation.c
+++ src/BitOperation.c
@@ -12,30 +12,36 @@
  */
 #define GET_MSBIT_8(byte)       ((byte) >> (8-1))
 
-// Assume for now that a uint8_t is exactly 8 bits wide.
-// Streamline this function to work only on 8-bit unsigned integers.
+/*
+ * Assume for now that a uint8_t is exactly 8 bits wide.
+ * Streamline this function to work only on 8-bit unsigned integers.
+ */
 int BitOperation_CircularShiftLeftOne(uint8_t *input, size_t input_len, uint8_t *output, size_t output_len)
 {
-    if (input_len == 1)
+    int i;
+
+    /*
+     * Bits wrap around between bytes:
+     * -------------------------------
+     * |                             |
+     * ^                             V
+     * 00000000 00000000 .. 0000000000
+     *        | |      |    |
+     *        ^ V      ^    V
+     *        ---      --..--
+     */
+    // Start at the end of the array
+    i = input_len - 1;
+
+    // Last byte is special - bit wraps around from the front of the array
+    output[i] = LEFT_SHIFT_ONE(input[i]) | GET_MSBIT_8(input[0]);
+
+    // Work backwards through the array
+    while (i > 0)
     {
-        // LSH(this) | MSB(first)
-        output[0] = LEFT_SHIFT_ONE(input[0]) | GET_MSBIT_8(input[0]);
-    }
-    else if (input_len == 2)
-    {
-        // LSH(this) | MSB(first)
-        output[1] = LEFT_SHIFT_ONE(input[1]) | GET_MSBIT_8(input[0]);
-        // LSH(this) | MSB(next)
-        output[0] = LEFT_SHIFT_ONE(input[0]) | GET_MSBIT_8(input[1]);
-    }
-    else if (input_len == 3)
-    {
-        // LSB(this) | MSB(first)
-        output[2] = LEFT_SHIFT_ONE(input[2]) | GET_MSBIT_8(input[0]);
-        // LSH(this) | MSB(next)
-        output[1] = LEFT_SHIFT_ONE(input[1]) | GET_MSBIT_8(input[2]);
-        // LSH(this) | MSB(next)
-        output[0] = LEFT_SHIFT_ONE(input[0]) | GET_MSBIT_8(input[1]);
+        --i;
+        // left_shift(this) | MSB(next)
+        output[i] = LEFT_SHIFT_ONE(input[i]) | GET_MSBIT_8(input[i+1]);
     }
 
     return 0;
