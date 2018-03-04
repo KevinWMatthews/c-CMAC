7b381fe8a696152fa04a7a67795ad73df6284d13
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-03-04 09:43:37 -0800

    Add skeleton of XOR bit operation

---
+++ tests/Test_Xor.cpp
@@ -0,0 +1,22 @@
+extern "C"
+{
+}
+
+#include "CppUTest/TestHarness.h"
+#include "CppUTestExt/MockSupport.h"
+
+TEST_GROUP(Xor)
+{
+    void setup()
+    {
+    }
+
+    void teardown()
+    {
+    }
+};
+
+TEST(Xor, wiring_check)
+{
+    FAIL("Start here");
+}
