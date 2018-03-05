d73c89e14fb8dd8b850db482d053922e9ac78ac7
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-03-04 17:22:43 -0800

    Create test framework for CMAC step 2

---
+++ tests/Test_CalculateNBlocks.cpp
@@ -0,0 +1,22 @@
+extern "C"
+{
+}
+
+#include "CppUTest/TestHarness.h"
+#include "CppUTestExt/MockSupport.h"
+
+TEST_GROUP(CalculateNBlocks)
+{
+    void setup()
+    {
+    }
+
+    void teardown()
+    {
+    }
+};
+
+TEST(CalculateNBlocks, wiring_check)
+{
+    FAIL("start here");
+}
