9c8a86df14673fd9096ee336008a248d1185716c
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-03-07 07:57:24 -0800

    Add a first stab at documentation for CmacOps

--- src/CmacOps.h
+++ src/CmacOps.h
@@ -5,12 +5,59 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+/* Calculate the number of blocks in a message and if the last block is complete..
+ *
+ * Each block is 128 bits (16 bytes).
+ * If the message length is precisely divisible by 16, the is complete block flag is not set.
+ * If the message contains trailing bits, the final block is considered to be incomplete.
+ * The block number is rounded up and and the is complete block flag is set.
+ * For example,
+ * 128 bits => 1 block, is_complete_block_flag = 0
+ * 129 bits => 2 blcoks, is_complete_block_flag = 1
+ * If the message length is 0, the number of blocks is set to 1.
+ *
+ * See RFC4493 for specification.
+ */
 size_t calculate_n_blocks(size_t message_length);
+//TODO combine these functions
 int set_is_complete_block(size_t *n_blocks, bool *is_complete_block_flag);
+
+/* Get the nth block in the message
+ *
+ * For the given message M, get nth block where n is given by the (1-indexed!) block number.
+ * Value is returned in M_n.
+ */
 int get_nth_block(uint8_t M[16], size_t M_len, size_t block_num, uint8_t M_n[16]);
+
+/* Calculate the XOR of the last message block.
+ *
+ * Given the nth block of the message, calculate the XOR of last block.
+ * This is used as a special input for the final step of the CMAC calculation.
+ *
+ * If the block is incomplete, XOR using K2.
+ *TODO if the block is complete, XOR using K1.
+ */
 int set_last_block_for_incomplete(uint8_t M_n[16], uint8_t K2[16], uint8_t M_last[16]);
+// Later will have set_last_block_for_complete() or something
+
+/* Apply the basic CMC-MAC algorithm to all but the last block of the message.
+ *
+ * XORs and AES-128 encrypts each block (according to the CBC-MAC spec?).
+ * The AES-CMAC is still incomplete; the two finishing steps must be applied.
+ */
 int apply_cbc_mac(uint8_t aes_key[16], uint8_t *message, size_t n_blocks, uint8_t X[16], uint8_t Y[16]);
+
+/* Apply first finishing step of the CBC-MAC.
+ *
+ * The last block is a special case: use M_last instead of M_n.
+ */
 int finish_cbc_mac_1(uint8_t M_last[16], uint8_t X[16], uint8_t Y[16]);
+
+/* Apply second finishing step of the CBC-MAC.
+ *
+ * Perform final AES-128 encryption.
+ * T is the CMC-MAC result.
+ */
 int finish_cbc_mac_2(uint8_t aes_key[16], uint8_t Y[16], uint8_t T[16]);
 
 #endif
