668fc884faac4801468907ae8e8c18dde730e02f
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-03-16 09:16:33 -0700

    Put in beginning of demo app

--- tests/Libgcrypt/TestApp_Libgcrypt.c
+++ tests/Libgcrypt/TestApp_Libgcrypt.c
@@ -1,4 +1,14 @@
+#include <gcrypt.h>
+
 int main(void)
 {
+    const char *actual_version = NULL;
+
+    actual_version = gcry_check_version("1.8.2");
+
+    printf("actual_version: %s\n", actual_version);
+
+    gcry_control(GCRYCTL_DISABLE_SECMEM, 0);
+
     return 0;
 }
