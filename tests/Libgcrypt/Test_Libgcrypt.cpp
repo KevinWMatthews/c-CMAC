3a5895fafe80d725d411c7e954a5e475091172fc
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-03-17 09:27:43 -0700

    Check behavior of libgcrypt gcry_check_version()

--- tests/Libgcrypt/Test_Libgcrypt.cpp
+++ tests/Libgcrypt/Test_Libgcrypt.cpp
@@ -16,7 +16,30 @@ TEST_GROUP(Libgcrypt)
     }
 };
 
-TEST(Libgcrypt, wiring_check)
+#define LIBGCRYPT_VERSION       "1.8.2"
+TEST(Libgcrypt, check_version_succeeds_with_version_match)
 {
-    FAIL("start here");
+    const char *required = LIBGCRYPT_VERSION;
+    const char *actual = NULL;
+
+    actual = gcry_check_version(required);
+    STRCMP_EQUAL( LIBGCRYPT_VERSION, actual );
+}
+
+TEST(Libgcrypt, check_version_returns_null_if_actual_version_is_too_old)
+{
+    const char *required = "1.8.3";
+    const char *actual = NULL;
+
+    actual = gcry_check_version(required);
+    STRCMP_EQUAL( NULL, actual );
+}
+
+TEST(Libgcrypt, check_version_succeeds_if_actual_version_is_newer_than_required)
+{
+    const char *required = "1.8.1";
+    const char *actual = NULL;
+
+    actual = gcry_check_version(required);
+    STRCMP_EQUAL( LIBGCRYPT_VERSION, actual );
 }
