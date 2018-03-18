1587d6af545ae8c137e72c9d33c39b037bb9db53
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-03-18 07:57:39 -0700

    Implement isEqual for Aes128 CreateParams comparator

--- mocks/Aes128CreateParamsComparator.cpp
+++ mocks/Aes128CreateParamsComparator.cpp
@@ -7,6 +7,26 @@ extern "C"
 
 bool Aes128CreateParamsComparator::isEqual(const void* object1, const void* object2)
 {
+    const AES128_CREATE_PARAMS *params1 = (const AES128_CREATE_PARAMS *)object1;
+    const AES128_CREATE_PARAMS *params2 = (const AES128_CREATE_PARAMS *)object2;
+
+    if (params1 == NULL)
+        return false;
+    if (params2 == NULL)
+        return false;
+
+    SimpleString key1 = StringFromBinaryWithSize(params1->key, params1->key_len);
+    SimpleString key2 = StringFromBinaryWithSize(params2->key, params2->key_len);
+
+    if (key1 != key2)
+        return false;
+
+    SimpleString iv1 = StringFromBinaryWithSize(params1->iv, params1->iv_len);
+    SimpleString iv2 = StringFromBinaryWithSize(params2->iv, params2->iv_len);
+
+    if (iv1 != iv2)
+        return false;
+
     return true;
 }
 
--- tests/mocks/Test_Aes128CreateParamsComparator.cpp
+++ tests/mocks/Test_Aes128CreateParamsComparator.cpp
@@ -11,6 +11,7 @@ TEST_GROUP(Aes128_CreateParamsComparator)
 {
     Aes128CreateParamsComparator comparator;
     AES128_CREATE_PARAMS params;
+    AES128_CREATE_PARAMS params2;
     SimpleString simple_string;
     const char *string;
 
@@ -61,4 +62,97 @@ TEST(Aes128_CreateParamsComparator, object_to_string_shows_size_and_contents_of_
     simple_string = comparator.valueToString(&params);
     string = simple_string.asCharString();
     STRCMP_CONTAINS("iv: Size = 16 | HexContent", string);
+}
+
+TEST(Aes128_CreateParamsComparator, compare_false_with_null_pointers)
+{
+    CHECK_FALSE( comparator.isEqual(NULL, &params2) );
+    CHECK_FALSE( comparator.isEqual(&params, NULL) );
+}
+
+TEST(Aes128_CreateParamsComparator, compare_true_with_empty_params)
+{
+    CHECK_TRUE( comparator.isEqual(&params, &params2) );
+}
+
+TEST(Aes128_CreateParamsComparator, compare_true_with_zero_length_key_and_iv)
+{
+    params.key_len = 0;
+    params.iv_len = 0;
+    params2.key_len = 0;
+    params2.iv_len = 0;
+    CHECK_TRUE( comparator.isEqual(&params, &params2) );
+}
+
+TEST(Aes128_CreateParamsComparator, compare_true_with_actual_key_and_iv)
+{
+    uint8_t key[16] = {};
+    uint8_t iv[16] = {};
+
+    params.key = key;
+    params.iv = iv;
+    params.key_len = 0;
+    params.iv_len = 0;
+
+    params2.key = key;
+    params2.iv = iv;
+    params2.key_len = 0;
+    params2.iv_len = 0;
+    CHECK_TRUE( comparator.isEqual(&params, &params2) );
+}
+
+TEST(Aes128_CreateParamsComparator, compare_false_if_key_len_is_different)
+{
+    uint8_t key[16] = {};
+    uint8_t key2[15] = {};
+
+    params.key = key;
+    params.key_len = sizeof(key);
+
+    params2.key = key2;
+    params2.key_len = sizeof(key2);
+
+    CHECK_FALSE( comparator.isEqual(&params, &params2) );
+}
+
+TEST(Aes128_CreateParamsComparator, compare_false_if_iv_len_is_different)
+{
+    uint8_t iv[16] = {};
+    uint8_t iv2[15] = {};
+
+    params.iv = iv;
+    params.iv_len = sizeof(iv);
+
+    params2.iv = iv2;
+    params2.iv_len = sizeof(iv2);
+
+    CHECK_FALSE( comparator.isEqual(&params, &params2) );
+}
+
+TEST(Aes128_CreateParamsComparator, compare_false_if_key_content_is_different)
+{
+    uint8_t key[16] = {0x00};
+    uint8_t key2[16] = {0x01};
+
+    params.key = key;
+    params.key_len = sizeof(key);
+
+    params2.key = key2;
+    params2.key_len = sizeof(key2);
+
+    CHECK_FALSE( comparator.isEqual(&params, &params2) );
+}
+
+TEST(Aes128_CreateParamsComparator, compare_false_if_iv_content_is_different)
+{
+    uint8_t iv[16] = {0x00};
+    uint8_t iv2[16] = {0x01};
+
+    params.iv = iv;
+    params.iv_len = sizeof(iv);
+
+    params2.iv = iv2;
+    params2.iv_len = sizeof(iv2);
+
+    CHECK_FALSE( comparator.isEqual(&params, &params2) );
 }
