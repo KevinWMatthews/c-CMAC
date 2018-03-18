27c5b308e7a5ed15d05747f87847291b7e04f04c
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-03-18 15:08:23 -0700

    Placeholder - CreateParams comparator segfaults with NULL pointer

--- mocks/Aes128CryptoParamsComparator.cpp
+++ mocks/Aes128CryptoParamsComparator.cpp
@@ -4,6 +4,7 @@ extern "C"
 }
 
 #include "Aes128CryptoParamsComparator.h"
+#include "Aes128CreateParamsComparator.h"
 
 bool Aes128CryptoParamsComparator::isEqual(const void* object1, const void* object2)
 {
@@ -11,5 +12,12 @@ bool Aes128CryptoParamsComparator::isEqual(const void* object1, const void* obje
 
 SimpleString Aes128CryptoParamsComparator::valueToString(const void* object)
 {
-    return NULL;
+    const AES128_CRYPTO_PARAMS *params = (const AES128_CRYPTO_PARAMS *)object;
+
+    SimpleString aes_handle;
+    Aes128CreateParamsComparator comparator;
+    // aes_handle = comparator.valueToString(NULL);//params->aes_handle);
+    SimpleString input = StringFromBinaryWithSize(params->input, params->input_len);
+
+    return aes_handle + StringFrom(", input: ") + input;
 }
--- tests/mocks/Test_Aes128CryptoParamsComparator.cpp
+++ tests/mocks/Test_Aes128CryptoParamsComparator.cpp
@@ -10,6 +10,9 @@ extern "C"
 TEST_GROUP(Aes128_CryptoParamsComparator)
 {
     Aes128CryptoParamsComparator comparator;
+    AES128_CRYPTO_PARAMS params;
+    SimpleString simple_string;
+    const char *string;
 
     void setup()
     {
@@ -27,4 +30,13 @@ TEST_GROUP(Aes128_CryptoParamsComparator)
 TEST(Aes128_CryptoParamsComparator, create_and_install_comparator)
 {
     mock().installComparator("AES128_CRYPTO_PARAMS", comparator);
+}
+
+IGNORE_TEST(Aes128_CryptoParamsComparator, object_to_string_shows_all_elements)
+{
+    simple_string = comparator.valueToString(&params);
+    string = simple_string.asCharString();
+
+    STRCMP_CONTAINS("AES handle:", string);
+    STRCMP_CONTAINS("input:", string);
 }
