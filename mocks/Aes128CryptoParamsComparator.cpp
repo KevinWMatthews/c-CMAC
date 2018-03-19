cc1c0238cc4ad8315e21776127fa90e28f03a987
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-03-18 17:43:56 -0700

    Extent CryptoParams comparator

--- mocks/Aes128CryptoParamsComparator.cpp
+++ mocks/Aes128CryptoParamsComparator.cpp
@@ -7,6 +7,24 @@ extern "C"
 
 bool Aes128CryptoParamsComparator::isEqual(const void* object1, const void* object2)
 {
+    const AES128_CRYPTO_PARAMS *params1 = (const AES128_CRYPTO_PARAMS *)object1;
+    const AES128_CRYPTO_PARAMS *params2 = (const AES128_CRYPTO_PARAMS *)object2;
+
+    if (object1 == NULL)
+        return false;
+    if (object2 == NULL)
+        return false;
+
+    SimpleString input1 = StringFromBinaryWithSize(params1->input, params1->input_len);
+    SimpleString input2 = StringFromBinaryWithSize(params2->input, params2->input_len);
+
+    if (input1 != input2)
+        return false;
+
+    if ( !createComparator.isEqual(params1->aes_handle, params2->aes_handle) )
+        return false;
+
+    return true;
 }
 
 SimpleString Aes128CryptoParamsComparator::valueToString(const void* object)
--- tests/mocks/Test_Aes128CryptoParamsComparator.cpp
+++ tests/mocks/Test_Aes128CryptoParamsComparator.cpp
@@ -12,13 +12,13 @@ TEST_GROUP(Aes128_CryptoParamsComparator)
 {
     Aes128CryptoParamsComparator comparator;
     AES128_CRYPTO_PARAMS params;
+    AES128_CRYPTO_PARAMS params2;
     SimpleString simple_string;
     const char *string;
 
     void setup()
     {
         mock().strictOrder();
-        memset( &params, 0, sizeof(params) );
     }
 
     void teardown()
@@ -64,4 +64,87 @@ TEST(Aes128_CryptoParamsComparator, object_to_string_shows_input_values)
     STRCMP_CONTAINS("AES128_CRYPTO_PARAMS", string);
     STRCMP_CONTAINS("input:", string);
     STRCMP_CONTAINS("00 01 02", string);
+}
+
+TEST(Aes128_CryptoParamsComparator, compare_false_with_null_pointers)
+{
+    CHECK_FALSE( comparator.isEqual(NULL, &params2) );
+    CHECK_FALSE( comparator.isEqual(&params, NULL) );
+}
+
+IGNORE_TEST(Aes128_CryptoParamsComparator, compare_true_with_empty_params)
+{
+    CHECK_TRUE( comparator.isEqual(&params, &params2) );
+}
+
+TEST(Aes128_CryptoParamsComparator, compare_false_with_different_input)
+{
+    uint8_t input1[16] = {0x01};
+    uint8_t input2[16] = {};
+
+    params.input = input1;
+    params.input_len = sizeof(input1);
+
+    params.input = input2;
+    params.input_len = sizeof(input2);
+
+    CHECK_FALSE( comparator.isEqual(&params, &params2) );
+}
+
+TEST(Aes128_CryptoParamsComparator, compare_true_with_equivalent_aes_handle)
+{
+    AES128_HANDLE aes_handle1 = NULL;
+    AES128_HANDLE aes_handle2 = NULL;
+    uint8_t key[16] = {};
+    uint8_t iv[16] = {};
+    uint8_t input[16] = {};
+
+    AES128_CREATE_PARAMS create_params = {};
+
+    create_params.key = key;
+    create_params.key_len = sizeof(key);
+    create_params.iv = iv;
+    create_params.iv_len = sizeof(iv);
+    Aes128_Create(&create_params, &aes_handle1);
+    Aes128_Create(&create_params, &aes_handle2);
+
+    params.aes_handle = aes_handle1;
+    params.input = input;
+    params.input_len = sizeof(input);
+
+    params2.aes_handle = aes_handle2;
+    params2.input = input;
+    params2.input_len = sizeof(input);
+
+    CHECK_TRUE( comparator.isEqual(&params, &params2) );
+}
+
+IGNORE_TEST(Aes128_CryptoParamsComparator, compare_false_with_different_aes_handle)
+{
+    AES128_HANDLE aes_handle1 = NULL;
+    AES128_HANDLE aes_handle2 = NULL;
+    uint8_t key[16] = {};
+    uint8_t iv[16] = {};
+    uint8_t input[16] = {};
+
+    AES128_CREATE_PARAMS create_params = {};
+
+    create_params.key = key;
+    create_params.key_len = sizeof(key);
+    create_params.iv = iv;
+    create_params.iv_len = sizeof(iv);
+    Aes128_Create(&create_params, &aes_handle1);
+
+    key[0] = 0x42;
+    Aes128_Create(&create_params, &aes_handle2);
+
+    params.aes_handle = aes_handle1;
+    params.input = input;
+    params.input_len = sizeof(input);
+
+    params2.aes_handle = aes_handle2;
+    params2.input = input;
+    params2.input_len = sizeof(input);
+
+    CHECK_FALSE( comparator.isEqual(&params, &params2) );
 }
