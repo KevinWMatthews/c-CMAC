ea334d836b6cae7c457787826df2889e952a073e
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-03-19 09:01:07 -0700

    Mock calls for new encryption interface
    Dummy call - do the encryption locally to verify
    that mocking works.

--- mocks/Mock_Aes128.cpp
+++ mocks/Mock_Aes128.cpp
@@ -14,7 +14,7 @@ AES128_RETURN_CODE Aes128_Initialize(void)
 AES128_RETURN_CODE Aes128_Create(AES128_CREATE_PARAMS *params, AES128_HANDLE *aes_handle)
 {
     mock().actualCall("Aes128_Create")
-        .withParameter("params", params)
+        .withParameterOfType("AES128_CREATE_PARAMS", "params", params)
         .withParameter("aes_handle", aes_handle);
     return (AES128_RETURN_CODE)mock().intReturnValue();
 }
