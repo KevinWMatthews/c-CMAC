cac05bb03abe76664372f72f1ab20248b2afb2a4
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-03-21 09:04:45 -0700

    Review: Create params struct for AesCmac calculation
    Which is preferable? Six arguments or a struct?

--- src/AesCmac.c
+++ src/AesCmac.c
@@ -64,3 +64,64 @@ int AesCmac_Calculate128(uint8_t key[16], size_t key_len,
 
     return 0;
 }
+
+int AesCmac_Calculate128_2(AES_CMAC_CALCULATE_128_PARAMS *params, uint8_t aes_cmac[16], size_t aes_cmac_len)
+{
+
+    unsigned char K1[16] = {0};
+    unsigned char K2[16] = {0};
+    size_t n_blocks;
+    bool is_complete_block;
+    int ret;
+
+    uint8_t iv[16] = {0};       // CMAC uses an IV of zeros.
+    AES128_HANDLE aes_handle = {0};
+    AES128_CREATE_PARAMS create_params = {
+        .key = params->key,
+        .key_len = params->key_len,
+        .iv = iv,
+        .iv_len = sizeof(iv),
+    };
+
+    Aes128_Initialize();
+    Aes128_Create(&create_params, &aes_handle);
+
+    // Step 1
+    ret = AesCmac_GenerateSubkeys( aes_handle,
+            K1, sizeof(K1),
+            K2, sizeof(K2) );
+
+    // Step 2
+    n_blocks = CmacOps_GetNBlocks(params->message_len);
+
+    // Step 3
+    is_complete_block = CmacOps_GetIsCompleteBlock(params->message_len);
+
+    // Step 4
+    // Given a message of n blocks, get the nth block.
+    // For now we have a zero-length message, so it will be all padding.
+    unsigned char M_n[16] = {0};
+    unsigned char M_last[16] = {0};
+
+    ret = CmacOps_GetNthBlock(params->message, params->message_len, n_blocks, M_n);
+    ret = CmacOps_SetLastBlockForIncomplete(M_n, K2, M_last);
+
+    // Step 5
+    unsigned char X[16] = {0};
+
+    // Step 6
+    unsigned char Y[16] = {0};
+    ret = CmacOps_ApplyCbcMac(params->key, params->message, n_blocks, X, Y);
+    ret = CmacOps_FinishCbcMac1(M_last, X, Y);
+
+
+    unsigned char T[16] = {0};
+    ret = CmacOps_FinishCbcMac2(aes_handle, Y, T, sizeof(T));
+
+    Aes128_Destroy(&aes_handle);
+
+    // Step 7
+    memcpy(aes_cmac, T, 16);
+
+    return 0;
+}
--- tests/Cmac/Test_AesCmac.cpp
+++ tests/Cmac/Test_AesCmac.cpp
@@ -40,3 +40,28 @@ TEST(AesCmac, rfc_4493_example_1_message_length_0)
     LONGS_EQUAL( ret, 0 );
     MEMCMP_EQUAL( expected, cmac, sizeof(expected) );
 }
+
+TEST(AesCmac, rfc_4493_example_1_message_length_0__)
+{
+    unsigned char expected[] = {
+        0xbb, 0x1d, 0x69, 0x29, 0xe9, 0x59, 0x37, 0x28,
+        0x7f, 0xa3, 0x7d, 0x12, 0x9b, 0x75, 0x67, 0x46,
+    };
+    unsigned char key[] = {
+        0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6,
+        0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c,
+    };
+    unsigned char *message = NULL;
+    size_t message_len = 0;
+    AES_CMAC_CALCULATE_128_PARAMS params = {};
+    unsigned char cmac[16] = {};
+
+    params.key = key;
+    params.key_len = sizeof(key);
+    params.message = message;
+    params.message_len = message_len;
+    ret = AesCmac_Calculate128_2( &params, cmac, sizeof(cmac) );
+
+    LONGS_EQUAL( ret, 0 );
+    MEMCMP_EQUAL( expected, cmac, sizeof(expected) );
+}
