6bc02517d3bad59d0df412103b446e6d5ac49a1c
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-03-24 16:10:13 -0700

    Change variable type in tests
    Correspond with the convention of the project

--- tests/Cmac/Test_CmacAes.cpp
+++ tests/Cmac/Test_CmacAes.cpp
@@ -23,18 +23,18 @@ static uint8_t zeros[16] = {};
 
 TEST(CmacAes, rfc_4493_example_1_message_length_0)
 {
-    unsigned char expected[] = {
+    uint8_t expected[] = {
         0xbb, 0x1d, 0x69, 0x29, 0xe9, 0x59, 0x37, 0x28,
         0x7f, 0xa3, 0x7d, 0x12, 0x9b, 0x75, 0x67, 0x46,
     };
-    unsigned char key[] = {
+    uint8_t key[] = {
         0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6,
         0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c,
     };
-    unsigned char *message = NULL;
+    uint8_t *message = NULL;
     size_t message_len = 0;
     CMAC_AES_CALCULATE_PARAMS params = {};
-    unsigned char cmac[16] = {};
+    uint8_t cmac[16] = {};
 
     params.key = key;
     params.key_len = sizeof(key);
