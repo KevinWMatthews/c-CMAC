ea36bf64b67d7e4bd743f653ec6f3a16005e4ed9
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-03-31 18:47:57 -0700

    Compile production code with -Wall

--- src/CmacAes.c
+++ src/CmacAes.c
@@ -5,47 +5,36 @@
 int CmacAes_Calculate(CMAC_AES_CALCULATE_PARAMS *params, uint8_t aes_cmac[16], size_t aes_cmac_len)
 {
     CMAC_AES_CONTEXT context = {0};
-    bool is_complete_block;
-    int ret;
-
-
-    uint8_t iv[16] = {0};       // CMAC uses an IV of zeros.
-    AES128_CREATE_PARAMS create_params = {
-        .key = params->key,
-        .key_len = params->key_len,
-        .iv = iv,
-        .iv_len = sizeof(iv),
-    };
 
     // Setup
-    ret = CmacAesOps_Initialize(&context, params->key, params->key_len);
+    CmacAesOps_Initialize(&context, params->key, params->key_len);
 
     // Step 1
-    ret = CmacAesOps_GenerateSubkeys(&context);
+    CmacAesOps_GenerateSubkeys(&context);
 
     // Step 2
-    ret = CmacAesOps_GetNBlocks(params->message_len, &context);
+    CmacAesOps_GetNBlocks(params->message_len, &context);
 
     // Step 3
-    ret = CmacAesOps_GetIsCompleteBlock(params->message_len, &context);
+    CmacAesOps_GetIsCompleteBlock(params->message_len, &context);
 
     // Step 4
-    ret = CmacAesOps_GetNthBlock(params->message, params->message_len, &context);
-    ret = CmacAesOps_SetLastBlockFromNthBlock(&context);
+    CmacAesOps_GetNthBlock(params->message, params->message_len, &context);
+    CmacAesOps_SetLastBlockFromNthBlock(&context);
 
     // Step 5
-    ret = CmacAesOps_InitializeCipherOutputBlock(&context);
+    CmacAesOps_InitializeCipherOutputBlock(&context);
 
     // Step 6
     //TODO Pull all of this into a function?
-    ret = CmacAesOps_ApplyCbcXor(&context);
-    ret = CmacAesOps_ApplyCbcAes(&context);
+    CmacAesOps_ApplyCbcXor(&context);
+    CmacAesOps_ApplyCbcAes(&context);
 
     // Step 7
-    ret = CmacAesOps_CopyCmacOutput(&context, aes_cmac, aes_cmac_len);
+    CmacAesOps_CopyCmacOutput(&context, aes_cmac, aes_cmac_len);
 
     // Teardown
-    ret = CmacAesOps_Unitialize(&context);
+    CmacAesOps_Unitialize(&context);
 
     return 0;
 }
