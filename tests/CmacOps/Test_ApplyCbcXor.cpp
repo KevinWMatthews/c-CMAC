3b5d487ffeca62e582d7c860f98e4676e6c392bf
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-04-12 08:54:18 -0700

    Create new test file for applying CbcXor

---
+++ tests/CmacOps/Test_ApplyCbcXor.cpp
@@ -0,0 +1,22 @@
+extern "C"
+{
+#include "CmacAesOps.h"
+}
+
+#include "CppUTest/TestHarness.h"
+
+TEST_GROUP(ApplyCbcXor)
+{
+    void setup()
+    {
+    }
+
+    void teardown()
+    {
+    }
+};
+
+TEST(ApplyCbcXor, wiring_check)
+{
+    FAIL("flunk");
+}
