824ec8abfc9398892ac4a781def6d81d8a95b699
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-04-22 08:46:31 -0700

    Next failing test
    This is hard; doesn't feel right.

--- tests/CmacOps/Test_ApplyCbcMac.cpp
+++ tests/CmacOps/Test_ApplyCbcMac.cpp
@@ -12,6 +12,7 @@ TEST_GROUP(ApplyCbcMac)
 
     void setup()
     {
+        //TODO need to initialize AES keys and such
     }
 
     void teardown()
@@ -19,9 +20,33 @@ TEST_GROUP(ApplyCbcMac)
     }
 };
 
-TEST(ApplyCbcMac, apply_to_zero_length_message)
+static uint8_t zeros[16] = {};
+
+TEST(ApplyCbcMac, does_not_opeate_on_zero_length_message)
 {
     ret = CmacAesOps_ApplyCbcMac(&context, NULL, 0);
 
     LONGS_EQUAL( 0, ret );
+    LONGS_EQUAL( 0, context.current_block_number );
+    // MEMCMP_EQUAL( zeros, context.cipher_input_block, 16 );
+}
+
+TEST(ApplyCbcMac, does_not_operate_on_single_block_message)
+{
+    uint8_t message[16] = {};
+
+    ret = CmacAesOps_ApplyCbcMac(&context, message, sizeof(message));
+
+    LONGS_EQUAL( 0, ret );
+    LONGS_EQUAL( 0, context.current_block_number );
+}
+
+TEST(ApplyCbcMac, operates_on_first_block_of_two_block_message)
+{
+    uint8_t message[17] = {};
+
+    ret = CmacAesOps_ApplyCbcMac(&context, message, sizeof(message));
+
+    LONGS_EQUAL( 0, ret );
+    LONGS_EQUAL( 1, context.current_block_number );
 }
